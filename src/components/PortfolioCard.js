f3f612dbd1c44b08da566dbe3ccc245235a7a7e3
Mu'adz <muuadzz10@gmail.com>
2022-04-24 05:14:31 +0700

    fix portfolio card color

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,10 +1,9 @@
-import { Card } from "@mui/material";
 import "../styles/portfoliocard.css"
 
 export default function PorfolioCard(props) {
-    return <Card className="card">
+    return <div className="card">
         <div className="icon">{props.icon}</div>
         <h3 className="title">{props.text}</h3>
         <p className="description">{props.description}</p>
-    </Card>
+    </div>
 }
