7929a8d632669ae754c835fccd4f20db7a877e3b
Mu'adz <muuadzz10@gmail.com>
2022-04-24 03:52:49 +0700

    fix margins

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -3,7 +3,7 @@ import "../styles/portfoliocard.css"
 
 export default function PorfolioCard(props) {
     return <Card className="card">
-        <div>{props.icon}</div>
+        <div className="icon">{props.icon}</div>
         <h3 className="title">{props.text}</h3>
         <p className="description">{props.description}</p>
     </Card>
