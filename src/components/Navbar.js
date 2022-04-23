fe922bac13835bbfb57a9053fa9a0cf05712712d
Mu'adz <muuadzz10@gmail.com>
2022-04-23 21:30:44 +0700

    add end button in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import NavButton from "./NavButton";
 import "../styles/navbar.css"
+import { ShoppingCart } from "@mui/icons-material";
+import { Search } from "@mui/icons-material";
 
 export default function Navbar() {
 
     return <nav className="navigation">
-        <NavButton text="Demos" />
-        <NavButton text="Pages" />
-        <NavButton text="Portfolio" />
+        <div className="nav-buttons">
+            <NavButton text="Demos" />
+            <NavButton text="Pages" />
+            <NavButton text="Portfolio" />
+        </div>
+        <div className="end-buttons">
+            <button><ShoppingCart /></button>
+            <button><Search /></button>
+            <input type="text" /> 
+        </div>
     </nav>
 }
