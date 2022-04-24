7577292d4b9dc40fb3efc1047b9423361b651764
Mu'adz <muuadzz10@gmail.com>
2022-04-24 08:41:02 +0700

    fix expand behaviour

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,26 +7,27 @@ export default function Navbar() {
 
     const [isNavExpanded, setIsNavExpanded] = useState(false);
 
-
     return <nav className="navigation">
-        <div className="nav-buttons">
-            <NavButton text="Demos" />
-            <NavButton text="Pages" />
-            <NavButton text="Portfolio" />
+        <div className="nav-section">
+            <div className="nav-buttons">
+                <NavButton text="Demos" />
+                <NavButton text="Pages" />
+                <NavButton text="Portfolio" />
+            </div>
+            <div className="end-buttons">
+                <button><ShoppingCart /></button>
+                <button><Search /></button>
+                <input type="text" /> 
+            </div>
+            <button 
+                className="hamburger"
+                onClick={() => {
+                    setIsNavExpanded(!isNavExpanded);
+                }}
+            >
+                <Menu />
+            </button>
         </div>
-        <div className="end-buttons">
-            <button><ShoppingCart /></button>
-            <button><Search /></button>
-            <input type="text" /> 
-        </div>
-        <button 
-            className="hamburger"
-            onClick={() => {
-                setIsNavExpanded(!isNavExpanded);
-            }}
-        >
-            <Menu />
-        </button>
         <div className={isNavExpanded ? "nav-menu-expanded" : "nav-menu"}>
             <NavButton text="Demos" />
             <NavButton text="Pages" />
